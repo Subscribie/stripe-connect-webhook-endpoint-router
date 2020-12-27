ff467c904368c9d4ca1a4f7602daf4ece089f4e1
chrisjsimpson <chris@karmacomputing.co.uk>
2020-12-27 19:19:06 +0000

    default to 4 threads

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ set -euxo pipefail
 export FLASK_APP=main.py
 export FLASK_DEBUG=1
 
-exec uwsgi --http :80 --workers 1 --chdir /usr/src/app/ --mount /=main:app
+exec uwsgi --http :80 --workers 1 --threads 4 --chdir /usr/src/app/ --mount /=main:app
 
