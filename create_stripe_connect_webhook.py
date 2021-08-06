e13be62fdb599b3f2af3b7243a8a2d4be1dab25f
chrisjsimpson <chris.j.simpson@live.co.uk>
2021-08-06 19:54:16 +0100

    wip #21 document webhooks setup

--- create_stripe_connect_webhook.py
+++ create_stripe_connect_webhook.py
@@ -3,11 +3,17 @@ Create initial connect webhook listening only for
 the events required.
 
 Returns the endpoint secret, needed for Stripe webhook validation
+
+Note: Stripe livemode is *always* true for Stripe connect. Why?
+
+Because Stripe connect accounts can either be in live or test mode so
+the live/test mode config is moved to each Stipe connect account (and
+they can change their account to be in live or test mode).
 """
 
 import stripe
 
-stripe_api_key = input("Stripe api key:")
+stripe_api_key = input("Stripe secret api key:")
 endpoint_url = input("Webhook url:")
 
 stripe.api_key = stripe_api_key
